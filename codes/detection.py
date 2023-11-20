ff87e6793713a42170296598d70347c700c6f965
Garfieldlyf <1034645905@qq.com>
2023-11-20 21:46:19 +0800

    update

--- codes/detection.py
+++ codes/detection.py
@@ -11,7 +11,7 @@ import tensorflow_probability as tfp
 import numpy as np
 import matplotlib.pylab as plt
 from matplotlib.pyplot import plot, ion, show, savefig, cla, figure
-#%matplotlib inline
+plt.ion()
 import random
 import time
 import os
